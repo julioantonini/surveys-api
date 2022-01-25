0021aa2177a07dab1efd2d4d1eef402ea99bcef1
Julio <julioantonini@gmail.com>
2022-01-25 14:46:01 -0300

    feat: ensure accountmongorepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,15 @@
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AddAccountRepository } from "./../../../../data/protocols/add-account-repository";
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(accountData);
+    const account = Object.assign({}, accountData, {
+      id: result.insertedId.toString(),
+    });
+
+    return account;
+  }
+}

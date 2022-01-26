432df4798160dfb56a00b018d89171ae5e574bab
Julio <julioantonini@gmail.com>
2022-01-26 17:38:40 -0300

    refactor: connect to mongodb before integration tests

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,7 +5,6 @@ export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
   fg.sync("**/src/main/routes/**routes.ts").map(async (file) => {
-    console.debug({ file });
     (await import(`../../../${file}`)).default(router);
   });
 };

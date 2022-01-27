16220566c987ea716d50e56f609aea9a06675589
Julio <julioantonini@gmail.com>
2022-01-27 15:21:56 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,20 +2,33 @@ import { HttpRequest, HttpResponse } from "../../presentation/protocols";
 import { Controller } from "./../../presentation/protocols/controller";
 import { LogControllerDecorator } from "./log";
 
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: { name: "any_name" },
+      };
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+  return { sut, controllerStub };
+};
+
 describe("LogController Decorator", () => {
   test("Should call controller handle", async () => {
-    class ControllerStub implements Controller {
-      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: { name: "any_name" },
-        };
-        return new Promise((resolve) => resolve(httpResponse));
-      }
-    }
-    const controllerStub = new ControllerStub();
-    const handleSpy = jest.spyOn(controllerStub, "handle");
-    const sut = new LogControllerDecorator(controllerStub);
+    const { sut, controllerStub } = makeSut();
     const httpRequest = {
       body: {
         name: "any_name",
@@ -24,7 +37,7 @@ describe("LogController Decorator", () => {
         passwordConfirmation: "any_password",
       },
     };
-
+    const handleSpy = jest.spyOn(controllerStub, "handle");
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });

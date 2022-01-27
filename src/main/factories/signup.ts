8c7e5865dab6369131fa54d639424c5783eea9bd
Julio <julioantonini@gmail.com>
2022-01-27 15:17:26 -0300

    feat:  ensure logcontrollerdecorator calls controller with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,15 +1,22 @@
-import { AccountMongoRepository } from "./../../infra/db/mongodb/account-repository/account";
-import { BcryptAdapter } from "./../../infra/criptography/bcrypt-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 import { DbAddAccount } from "./../../data/usecases/add-account/db-add-account";
-import { EmailValidatorAdapter } from "./../../utils/email-validator-adapter";
+import { BcryptAdapter } from "./../../infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "./../../infra/db/mongodb/account-repository/account";
 import { SignUpController } from "./../../presentation/controllers/signup/signup";
+import { Controller } from "./../../presentation/protocols/controller";
+import { EmailValidatorAdapter } from "./../../utils/email-validator-adapter";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const salt = 12;
   const bcryptAdapter = new BcryptAdapter(salt);
   const accountMongoRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
 
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  const signUpController = new SignUpController(
+    emailValidatorAdapter,
+    dbAddAccount
+  );
+
+  return new LogControllerDecorator(signUpController);
 };

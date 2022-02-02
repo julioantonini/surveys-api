6b33b3b85194c4078d6c57dbcd2970c90d7d8546
Julio <julioantonini@gmail.com>
2022-02-02 17:33:23 -0300

    test: ensure jwtadapter throw if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -11,7 +11,7 @@ describe("Bcrypt Adapter", () => {
   test("should call sign with correct values", async () => {
     const sut = new JwtAdapter("secret");
     const signSpy = jest.spyOn(jwt, "sign");
-    sut.encrypt("any_id");
+    await sut.encrypt("any_id");
     expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret");
   });
 
@@ -19,5 +19,14 @@ describe("Bcrypt Adapter", () => {
     const sut = new JwtAdapter("secret");
     const accessToken = await sut.encrypt("any_id");
     expect(accessToken).toBe("any_token");
+  });
+
+  test("should throw if sign throws", async () => {
+    const sut = new JwtAdapter("secret");
+    jest.spyOn(jwt, "sign").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.encrypt("any_id");
+    await expect(promise).rejects.toThrow();
   });
 });

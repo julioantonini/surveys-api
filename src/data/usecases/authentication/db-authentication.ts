5eff9f5a7ae61a7f5710a0cd7aa7d5db435d0493
Julio <julioantonini@gmail.com>
2022-02-02 15:10:23 -0300

    feat: ensyre DbAuthentication calls  LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,18 @@
+import { LoadAccountByEmailRepository } from "./../../protocols/load-account-by-email-repository";
+import {
+  Authentication,
+  AuthenticationModel,
+} from "./../../../domain/usecases/authentication";
+
+export class DbAuthentication implements Authentication {
+  constructor(
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  ) {}
+
+  async auth(authentication: AuthenticationModel): Promise<string> {
+    const account = await this.loadAccountByEmailRepository.load(
+      authentication.email
+    );
+    return null;
+  }
+}
